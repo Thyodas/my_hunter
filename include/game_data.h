abcfb1247b1acc4fbcdf2110b6f6f5531a9b064e
Thyodas <guillaume.hein@epitech.eu>
2021-12-03 05:17:57 +0100

    Added include

--- include/game_data.h
+++ include/game_data.h
@@ -15,6 +15,8 @@
     #include <SFML/System/Export.h>
     #include <SFML/Graphics/Texture.h>
 
+    #include "my_list.h"
+
     /*typedef struct {
         int
     } texture_state_t;*/
@@ -35,13 +37,45 @@
         sfSprite *sprite;
         sfVector2f speed_vector;
         sfIntRect *state_list;
-        int state_index;
-        int id;
         int animation_counter;
+        int id;
     } sprite_data_t;
 
     typedef struct {
+        sfRenderWindow *window;
+        sfEvent event;
+        sfClock *clock;
         sprite_data_t *ship;
+        sprite_data_t *background;
+        linked_list_t *projectile_list;
     } game_data_t;
+
+    // main.c
+    int game_loop(game_data_t *data);
+    int my_hunter(void);
+    int main(int argc, char **argv);
+
+    // projectile.c
+    void move_projectile(sprite_data_t *projectile);
+    void animate_projectile(sprite_data_t *projectile);
+    sprite_data_t *create_projectile(game_data_t *g_data);
+    void render_all_projectile(game_data_t *g_data);
+
+    // ship.c
+    void move_ship(sprite_data_t *ship);
+    void animate_ship(sprite_data_t *ship);
+    void create_ship(game_data_t *g_data);
+
+    // background.c
+    void move_background(sprite_data_t *background);
+    void create_background(game_data_t *g_data);
+
+    // events.c
+    void window_events(game_data_t *g_data);
+    void zqsd_events(game_data_t *g_data);
+    void event_handler(game_data_t *g_data);
+
+    // print_help.c
+    void print_help(void);
 
 #endif /* !GAME_DATA_H_ */
